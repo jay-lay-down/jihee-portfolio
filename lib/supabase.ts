148bf9af525bc6dbc8fe829572c457df505653e7
Jiheecho <chubbyfinger1010@gmail.com>
2025-12-05 14:55:33 +0900

    Create supabase.ts

---
+++ lib/supabase.ts
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+// 아까 수파베이스 설정(Settings -> API)에서 본 값을 따옴표 안에 넣으세요.
+const supabaseUrl = 'https://wchcdfeyazvsibxbasbb.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndjaGNkZmV5YXp2c2lieGJhc2JiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4NzEzNDYsImV4cCI6MjA4MDQ0NzM0Nn0.v8gYCjS2gys9oBcDwEYhj-kSnDq1liDungsMFRdhMGE';
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
