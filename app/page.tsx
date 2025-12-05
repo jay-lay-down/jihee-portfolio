c5352ca66ddc3b50d67937e764be24b127870734
Jiheecho <chubbyfinger1010@gmail.com>
2025-12-05 11:44:29 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import HomeTabs from "@/components/HomeTabs";
 
 export default function Home() {
   return (
-    <main className="container-wide px-4 sm:px-6 lg:px-10 py-6 sm:py-10">
+    <main className="container-wide px-10">
       <HomeTabs />
     </main>
   );
